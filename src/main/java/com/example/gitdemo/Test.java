652aa1deb6af56efd51d927ad8dc2ebff1179f05
loozixuan <zixuanagmo@gmail.com>
2025-07-26 22:33:15 +0800

    commit

--- src/main/java/com/example/gitdemo/Test.java
+++ src/main/java/com/example/gitdemo/Test.java
@@ -3,6 +3,7 @@ package com.example.gitdemo;
 public class Test {
     public static final int GENERAL_CONFIG_DAY = 1;
     public static final int CONFIG_DAY = 1;
+    public static final int CONFIG_DAY_3 = 1;
 
     public void test(){
     }
